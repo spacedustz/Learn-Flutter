6c792ab4c3aada9a88b14b725e6ef6a29b25ab47
KunWoo Shin <cyberspecterr@gmail.com>
2023-05-29 21:53:49 +0900

    Learn SafeArea & Expanded & Flexible

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:learn_flutter/screen/row_column/training.dart';
 import 'package:learn_flutter/screen/splash_screen/splash_screen.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: SplashScreen()
+    home: Training()
   ));
 }
---
+++ lib/screen/row_column/home_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        bottom: false,
+        child: Container(
+          color: Colors.black,
+          height: MediaQuery.of(context).size.height,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Expanded(
+                child: Container(
+                  color: Colors.red,
+                  width: 50.0,
+                  height: 50.0,
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.orange,
+                  width: 50.0,
+                  height: 50.0,
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.yellow,
+                  width: 50.0,
+                  height: 50.0,
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.green,
+                  width: 50.0,
+                  height: 50.0,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
